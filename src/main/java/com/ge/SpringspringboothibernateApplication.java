e2131a94d20376f180a15539aeb5518ed46175b7
qiuyangyang <qiuyangyang@ge>
2017-11-28 17:38:09 +0800

    初始化版本

---
+++ src/main/java/com/ge/SpringspringboothibernateApplication.java
@@ -0,0 +1,12 @@
+package com.ge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringspringboothibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringspringboothibernateApplication.class, args);
+	}
+}
